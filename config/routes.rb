1bbd07c9cb82311b3d02d262563ad320c2f60845
sice11 <tensyoku.yonezawa@gmail.com>
2020-09-04 00:42:39 +0900

    user画面を作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+
+  get "users/new", to: "users#new", as: "users_new"
+  get "users/new_freeword", to: "users#new_freeword", as: "users_new_freeword"
+
+  get "users/index", to: "users#index", as: "users_index"
+
+  get "users/edit", to: "users#edit", as: "users_edit"
+
+  get "users/show", to: "users#show", as: "users_show"
+
   root "homes#top"
 
 end
